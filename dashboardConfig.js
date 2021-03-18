d1301b8997addc5f7d36d54cf47c1016f338d7fd
mamfi-0221 <mariel.filosopo@webriq.services>
2021-03-18 17:26:26 +0800

    added netlify deploy

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -1,6 +1,20 @@
 export default {
   widgets: [
     {
+      name: 'netlify',
+      options: {
+        title: 'Deployed to Netlify',
+        sites: [
+          {
+            name: 'page-builder-studio',
+            title: 'Page Builder',
+            apiId: 'skRTVxlXpbEhnkEfGSJUp8w4VzDgOyvuNQ8fWaMv57r8a2lj81dBGZ9tz63UBDaUnirSmL6dmw3lMqshMfjviXh1wLbW1AvKZifOwjlsXl2yUa8ixpAYTdTwigLAE7By7bdkhDR5UedTUOQwT1A2ZYDbXEcqM2g3yELQ7GkKHZbN6mNRg5zj',
+            buildHookId: '60531b9fdfb5aab1290added'
+          }
+        ]
+      }
+    },
+    {
       name: 'project-info',
       layout: {
         width: 'small',
